aa3c4f2a962155597bce0a238dd7d6c5afa9f05f
Rokas <rokas@osos.lt>
2018-05-16 17:59:32 +0300

    '3rd'

--- domparser.php
+++ domparser.php
@@ -16,9 +16,11 @@ $dom = HtmlDomParser::str_get_html( $page );
 // }
 
 foreach( $dom->find('div[class=widget panel]') as $job) {
-    $item['heading'] = $job->find('h4.media-heading', 0)->plaintext;
+    $item['title'] = $job->find('h4.media-heading', 0)->plaintext;
     $item['location'] = $job->find('p.location', 0)->plaintext;
     $item['date'] = $job->find('p.date', 0)->plaintext;
+    $item['content'] = trim($job->find('article', 0)->plaintext);
+    $item['apply_link'] = $job->find('p.text-center a', 0)->href;
     $jobs[] = $item;
 }
 
